db496f0f5f640200c8e3a99aea62784475067d3d
Elias Mercado Cantero <eliasmercan@msn.com>
2022-04-29 11:41:36 -0500

    se adiciono al proyecto la consulta de 3 imagenes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascript
 Repositorio orientado al estudio de Javascript
+
+## consumoapirestjavascript
+Proyecto de platzi donde se enseña al manejo de api rest con javascript
--- consumoapirestjavascript/main.js
+++ consumoapirestjavascript/main.js
@@ -1,12 +1,21 @@
 console.log('hello world')
 
-const API_URL='https://api.thecatapi.com/v1/images/search'
+//Link de consulta basica
+// const API_URL='https://api.thecatapi.com/v1/images/search'
+
+// Link con datos adicionales a la ruta para obtener mas datos de la api
+const API_URL='https://api.thecatapi.com/v1/images/search?limit=3'
 
 async function reload(){
     const res = await fetch(API_URL);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    console.log(data);
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
 reload();
