164832a2cbad6bad003fc75933dbefcf677c7cd7
Elias Mercado Cantero <eliasmercan@msn.com>
2022-06-19 11:48:59 -0500

    trabajando en nuevo modulo del proceso

--- masterenjavascript/02poo/js/03ejerciciosdelogica.js
+++ masterenjavascript/02poo/js/03ejerciciosdelogica.js
@@ -69,8 +69,42 @@ var b = {
     resultado: function(){
         if(b.Mac.tono > b.Smith.tono &&
            b.Mac.velocidad > b.Jack.velocidad &&
-           b.Mac.edad > b.Jack.edad){
+           b.Mac.edad > b.Jack.edad &&
+           b.Willy.velocidad > b.Jack.velocidad &&
+           b.Mac.edad > b.Willy.edad &&
+           b.Mac.edad > b.Smith.edad &&
+           b.Willy.tono > b.Jack.velocidad &&
+           b.Jack.tono > b.Smith.tono){
+
+            return true;
 
         }
+
+        return false;
     },
+
+    intervalo: setInterval(function(){
+        b.Mac.edad = Math.ceil(Math.random()*2);
+        b.Mac.velocidad = Math.ceil(Math.random()*2);
+        b.Mac.tono = Math.ceil(Math.random()*2);
+
+        b.Smith.edad = Math.ceil(Math.random()*2);
+        b.Smith.velocidad = Math.ceil(Math.random()*2);
+        b.Smith.tono = Math.ceil(Math.random()*2);
+
+        b.Jack.edad = Math.ceil(Math.random()*2);
+        b.Jack.velocidad = Math.ceil(Math.random()*2);
+        b.Jack.tono = Math.ceil(Math.random()*2);
+
+        b.Willy.edad = Math.ceil(Math.random()*2);
+        b.Willy.velocidad = Math.ceil(Math.random()*2);
+        b.Willy.tono = Math.ceil(Math.random()*2);
+
+        if(b.resultado()){}
+            clearInterval(b.intervalo);
+            console.log("Caballo de Mac", b.Mac);
+            console.log("Caballo de Smith", b.Smith);
+            console.log("Caballo de Jack", b.Jack);
+            console.log("Caballo de Willy", b.Willy);
+    }, 1)
 }
