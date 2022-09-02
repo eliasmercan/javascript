bc52f0e46768bb3aa0f5602b5f08bee117d81834
Elias Mercado Cantero <eliasmercan@msn.com>
2022-09-02 05:00:46 -0500

    trabajando en animacion del slide

--- masterenjavascript/04slideshow/js/slide.js
+++ masterenjavascript/04slideshow/js/slide.js
@@ -2,7 +2,9 @@
 var p={
     paginacion : document.querySelectorAll("#paginacion"),
     item: 0,
-    cajaSlide: document.querySelector("#slide ul")
+    cajaSlide: document.querySelector("#slide ul"),
+    animacionSlide: "fade",
+    imgSlide: document.querySelector("#slide ul li")
 }
 // Objeto con los metodos del slide
 var m={
@@ -18,8 +20,25 @@ var m={
     },
 
     movimientoSlide: function(item){
+        alert("HOLA item "+ item)
         p.cajaSlide.style.left = item* -100 +"%"
-        console.log(item* -100 +"%")
+        
+        for(var i=0; i < p.paginacion.length; i++){
+            p.paginacion[i].style.opacity = .5
+        }
+
+        p.paginacion[item].style.opacity = 1
+
+        if(p.animacionSlide=="slide"){
+            p.cajaSlide.style.transition =".7s left ease-in-out"
+        }
+
+        if(p.animacionSlide=="fade"){
+            alert("fade")
+            for(var i=0; i < p.imgSlide.length; i++){
+                p.paginacion[i].style.opacity = 0
+            }
+        }
 
     }
 }
