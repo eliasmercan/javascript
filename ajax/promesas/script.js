ddb1b63de41b76669e04a6fd32d244067528d599
Elias Mercado Cantero <eliasmercan@msn.com>
2022-11-06 18:26:26 -0500

    trabajando con promesas

---
+++ ajax/promesas/script.js
@@ -0,0 +1,39 @@
+const app = document.getElementById("app")
+const normal = document.getElementById("normal")
+const promesa = document.getElementById("promesa")
+const asyncawait = document.getElementById("asyncawait")
+
+const saludar = (name)=>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if (name === ""){
+                reject("El nombre no puede estar vacio")
+            }else{
+                resolve(`Hola ${name}`)
+            }
+        }, 2000)
+    })
+}
+
+const write = (data) =>{
+    app.innerText=data
+}
+
+normal.addEventListener("click", ()=>{
+    console.log("Empieza el proceso")
+    const resp = saludar("pedro")
+    write(resp)
+    console.log("Termina el proceso")
+})
+
+promesa.addEventListener("click", ()=>{
+    console.log("Empieza el proceso")
+    saludar("Edteam")
+    .then(response => write(response))
+    .catch(error => console.log(error))
+    console.log("Termina el proceso")
+})
+
+asyncawait.addEventListener("click", ()=>{
+
+})
