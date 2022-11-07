34ab97ab488d81d423a93593c1cd2b434277246c
Elias Mercado Cantero <eliasmercan@msn.com>
2022-11-06 22:02:21 -0500

    post con axios

--- ajax/axios/script.js
+++ ajax/axios/script.js
@@ -1,3 +1,32 @@
+const myForm = document.getElementById("myForm")
+const title = document.getElementById("title")
+const author = document.getElementById("author")
+const btnSave = document.getElementById("btnSave")
+
+myForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+})
+
+const makePost = () =>{
+    return {
+        title: title.value,
+        author: author.value
+    }
+}
+btnSave.addEventListener("click", async()=>{
+    const data = makePost()
+    try{
+        const response = await axios({
+            method: "post",
+            url: "http://localhost:3000/posts",
+            data: data
+        })
+        console.log(response)
+    }catch(error){
+        console.log(error)
+    }
+})
+
 //Metodo sencilo de uso
 // axios.get("http://localhost:3000/posts")
 // .then(response => console.log(response.data))
@@ -10,4 +39,6 @@ axios({
     method: "get",
 })
 .then(response => console.log(response.data))
-.catcfh(error => console.log(error.response))
+.catcfh(error => console.log(error))
+
+
