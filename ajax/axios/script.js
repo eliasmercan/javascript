fbe427bdfc3061ac5ad8f8f0a4239dabbe265332
Elias Mercado Cantero <eliasmercan@msn.com>
2022-11-06 21:37:12 -0500

    agregando modos de uso de axios

--- ajax/axios/script.js
+++ ajax/axios/script.js
@@ -1,3 +1,13 @@
-axios.get("http://localhost:3000/posts")
+//Metodo sencilo de uso
+// axios.get("http://localhost:3000/posts")
+// .then(response => console.log(response.data))
+// .catcfh(error => console.log(error.response))
+
+//Metodo con objeto con configuracion
+axios({
+    url:"/posts",
+    baseURL: "http://localhost:3000/",
+    method: "get",
+})
 .then(response => console.log(response.data))
-.catch(error => console.log(error.response))
+.catcfh(error => console.log(error.response))
