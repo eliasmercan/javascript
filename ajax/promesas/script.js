ffaaffd3eb67cd8b6b0c98f977238558830124b9
Elias Mercado Cantero <eliasmercan@msn.com>
2022-11-06 20:00:13 -0500

    consultando api con fetch - asyn&await

--- ajax/promesas/script.js
+++ ajax/promesas/script.js
@@ -2,6 +2,7 @@ const app = document.getElementById("app")
 const normal = document.getElementById("normal")
 const promesa = document.getElementById("promesa")
 const asyncawait = document.getElementById("asyncawait")
+const btnFetch = document.getElementById("btnFetch")
 
 const saludar = (name)=>{
     return new Promise((resolve, reject) => {
@@ -43,4 +44,21 @@ asyncawait.addEventListener("click", async()=>{
         console.log(error)
     }
     console.log("Termina el proceso")
+})
+
+btnFetch.addEventListener("click", async()=>{
+    /*Metodo tradicional de consultar una api con FECTH */
+    // fetch("http://localhost:3000/posts")
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+    // .catch(err => console.log(err) )
+
+    /* Metodo usando Asyn&Await*/
+    try{
+        const response = await fetch("http://localhost:3000/posts")
+        const data = await response.json()
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
 })
